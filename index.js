d6d1a36a2d82d2ef997ce615ac30f8f58c0a012e
igorNeves007 <igorneves0404@gmail.com>
2022-08-31 15:22:14 -0300

    Select PORT

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ app.use(express.json())
 app.use(cors())
 app.use(routes)
 
-app.listen(21262, () => {
+const PORT = process.env.PORT || 5050 // Variable that receives the port
+
+app.listen(PORT, () => {
   console.log('Server on port 21262')
 })
